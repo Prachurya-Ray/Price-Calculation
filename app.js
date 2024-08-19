d43c15837629d413e04216ff4adba49ff7507da9
Prachurya Ray <prachuryaray@gmail.com>
2024-08-19 20:08:49 +0530

    code cleaned

--- app.js
+++ app.js
@@ -1,47 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import Body from  "./Component/Body"
-import Header from "./Header_Component/Header"
-import BillingHeader from "./Billing/BillingHeader"
-import DineInMain from "./Dine-In/Main"
-import Orders from "./Operations/Orders/Orders"
-import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Error from "./Errors"
 
-// const AppLayout = ()=>{
-//     return(
-//         <> 
-//             <Orders/>
-//             <DineInMain/>
-//             <BillingHeader />
-//             <Header/>
-//             <h1 style={{textAlign:"center"}}>Choose Items</h1>
-//             <Body/>
-//         </>
-//     )
-// }
 
 const appRouter = createBrowserRouter([
     {
         path: "/",
-        element: <Header/>,
+        element: <Body/>,
         errorElement: <Error/>
-    },
-    {
-        path: "/dineIn",
-        element: <DineInMain/>,
-    },
-    {
-        path: "/bill",
-        element: <BillingHeader/>,
     },
     {
         path: "/calculation",
         element: <Body/>,
-    },
-    {
-        path: "/orders",
-        element: <Orders/>,
     },
 ])
 
@@ -49,3 +21,5 @@ const appRouter = createBrowserRouter([
 
 let root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<RouterProvider router={appRouter}/>)
+
+
